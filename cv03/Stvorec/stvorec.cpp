98c759413148f6f1d65d8f76742d8d1c191bb3c9
MiroslavKastier <272835@vutbr.cz>
2024-10-01 13:26:26 +0200

    Vytvoril som složku štvorec, kde som naprogramoval výpočet objemu a obsahu štvorca

---
+++ cv03/Stvorec/stvorec.cpp
@@ -0,0 +1,33 @@
+﻿// FunExt.cpp : Defines the entry point for the application.
+//
+
+#include "stvorec.h"
+
+using namespace std;
+
+int objem(int a)
+{
+    int ans = 4 * a;
+    return ans;
+}
+
+int obsah(int a)
+{
+    int ans = a * a;
+    return ans;
+}
+int main()
+{
+    int a;
+    printf("Zadaj stranu A štvorca v cm: ");
+    scanf("%d", &a);
+
+    int obsahStvorca = obsah(a);
+    int objemStvorca = objem(a);
+
+    printf("Obsah štvorca je: %d cm^2\nObjem štvorca je: %d cm\n", obsahStvorca, objemStvorca);
+    return 0;
+}
+
+
+

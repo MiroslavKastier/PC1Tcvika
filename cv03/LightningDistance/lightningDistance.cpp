1e908f7481a8d7aa52395649d41dced6fa82b1c0
MiroslavKastier <272835@vutbr.cz>
2024-10-01 13:14:07 +0200

    vytvoril som složku Lightning Distance podľa úlohy č. 5 a naprogramoval som ju

---
+++ cv03/LightningDistance/lightningDistance.cpp
@@ -0,0 +1,18 @@
+﻿// FunExt.cpp : Defines the entry point for the application.
+//
+
+#include "lightningDistance.h"
+
+using namespace std;
+
+int main()
+{
+    int time;
+    printf("Enter a time in seconds from lighting to thhunder: ");
+    scanf("%d", &time);
+
+    int ans = 340 * time;
+
+    printf("The distance of the lightning is: %d meters\n", ans);
+    return 0;
+}

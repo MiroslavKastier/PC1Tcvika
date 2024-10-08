7fd060164d234d697eb077137439868a1837567b
MiroslavKastier <272835@vutbr.cz>
2024-10-08 12:43:09 +0200

    vytvoril som a naimplementoval funkciu, ktorá zistí či je rok prestupný alebo nie

---
+++ cv04/PrestupnyRok/PrestupnyRok.cpp
@@ -0,0 +1,45 @@
+﻿// FunExt.cpp : Defines the entry point for the application.
+//
+
+#include "PrestupnyRok.h"
+
+using namespace std;
+
+int jePrestupny(int rok)
+{
+    if((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int rok;
+    int ans;
+
+    do
+    {
+        printf("Zadajte rok: ");
+        scanf("%d", &rok);
+
+        int prestupnyRok = jePrestupny(rok);
+
+        if (prestupnyRok == 1)
+        {
+            printf("\nRok %d je prestupný\n", rok);
+        }
+        else
+        {
+            printf("\nRok %d nie je prestupný\n", rok);
+        }
+
+        printf("\nAk chcete zadať ďalší rok, zadajte 1 (Ano) / 2 (Nie): ");
+        scanf("%d", &ans);
+
+    } while (ans == 1);
+    
+}
